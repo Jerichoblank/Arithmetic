418101c61be0c6f03b74fea4d40c84839bbaaf63
Jerichoblank <89247827+Jerichoblank@users.noreply.github.com>
2021-10-03 15:54:13 +0800

    Add files via upload
    A spinning tire makes 3000 revolutions per minutes. Calculate How many degrees it rotate in one second.
    Then display the Degrees.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int number_1 = 3000,
+         number_2 = 6,
+        result;
+    //multiplication
+    result = number_1 * number_2;
+    printf("%drpm * %ddps is %ddps", number_1, number_2, result);
+
+}
